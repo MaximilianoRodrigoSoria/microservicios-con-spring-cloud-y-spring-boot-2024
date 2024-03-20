3736242a337ac9e880f5db12541a37635aad7ec6
Maximiliano Rodrigo Soria <maximilianorodrigosoria@gmail.com>
2024-03-20 17:04:21 -0300

    CompanyController is added

---
+++ companies-crud/src/main/java/ar/com/laboratory/companiescrud/controller/CompanyController.java
@@ -0,0 +1,45 @@
+package ar.com.laboratory.companiescrud.controller;
+
+import ar.com.laboratory.companiescrud.entities.Company;
+import ar.com.laboratory.companiescrud.services.CompanyService;
+import jakarta.websocket.server.PathParam;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.rmi.NoSuchObjectException;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path="/api/v1/company")
+@Slf4j
+public class CompanyController {
+
+    private CompanyService companyService;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Company> get(@PathVariable String name) throws NoSuchObjectException {
+        log.info("GET company: {}", name);
+        return ResponseEntity.ok(companyService.readByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<Company> post(@RequestBody Company company){
+        log.info("POST company: {}", company.getName());
+        return ResponseEntity.created(URI.create(companyService.created(company).getName())).build();
+    }
+
+    @PutMapping ("/{name}")
+    public ResponseEntity<Company> put(@RequestBody Company company,@PathVariable String name) throws NoSuchObjectException {
+        log.info("PUT company: {}", name);
+        return ResponseEntity.ok(companyService.update(company, name));
+    }
+
+    @DeleteMapping ("/{name}")
+    public ResponseEntity<?> delete(@PathVariable String name) throws NoSuchObjectException {
+        log.info("DELETE company: {}", name);
+        return ResponseEntity.noContent().build();
+    }
+}
